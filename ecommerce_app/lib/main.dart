3c53331389267c303c2780304483d244ae38ef98
Khánh Nguyễn <nqkhanh2409@gmail.com>
2023-08-07 21:19:15 +0700

    NguyenQuocKhanh

---
+++ ecommerce_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '/routes.dart';
+// import '/screens/profile/profile_screen.dart';
+import '/screens/splash/splash_screen.dart';
+import '/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: theme(),
+      // home: SplashScreen(),
+      // We use routeName so that we dont need to remember the name
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
+    );
+  }
+}
